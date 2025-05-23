a2d7b5e884bdb04784fc9cfc43b99999102c55f3
Lizel Potgieter <lizelpotgieter@gmail.com>
2025-05-23 13:10:58 +0200

    Create tidy_taxonomy.sh

---
+++ tidy_taxonomy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Input CSV file
+INPUT="ncbi_lineages_2025-05-23.csv"
+WORKFILE="work"
+OUTPUT="tidied_taxonomy"
+
+awk '{print $1}' "$INPUT" > "$WORKFILE"
+
+awk '{print $1}' ncbi_lineages_2025-05-23.csv > "$WORKFILE"
+sed -i 's/,/;k__/1' "$WORKFILE"
+sed -i 's/,/;p__/1' "$WORKFILE"
+sed -i 's/,/;c__/1' "$WORKFILE"
+sed -i 's/,/;o__/1' "$WORKFILE"
+sed -i 's/,/;f__/1' "$WORKFILE"
+sed -i 's/,/;g__/1' "$WORKFILE"
+sed -i 's/,/;s__/1' "$WORKFILE"
+sed -i 's/,.*//' "$WORKFILE"
+sed 's/\(;[a-z]__\)\(;.*\)\{0,\}$//' "$WORKFILE" > "$OUTPUT"
